45ae52a85e4b4a5dedfd40f485721be19bf7ec2b
ageorge624 <ageorge624@gmail.com>
2024-09-22 16:57:22 -0500

    Automated commit

---
+++ AGit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Replace "Your commit message here" with your desired message
+commit_message="Automated commit"
+
+# Add all changes to the staging area
+git add .
+
+# Commit the changes with the specified message
+git commit -m "$commit_message"
+
+# Push the changes to the remote repository
+(replace "origin" with your remote name)
+git push ageorge
