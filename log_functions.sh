fa270a7e072e0f067b73379659c0cdebbab8661e
ageorge224 <ageorge224@gmail.com>
2024-10-04 08:43:03 -0500

    Automated commit

--- log_functions.sh
+++ log_functions.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
 # Variables
-
+VERSION="1.2.9"
 SCRIPT_NAME="local_update.sh"
 REMOTE_USER="ageorge"
-REMOTE_HOST="192.168.1.248"
+pihole="192.168.1.248"
+pihole2="192.168.1.145"
+AG_backup="192.168.1.238"
+BACKUP_LOG_DIR="/home/ageorge/.local_update_logs"
 BACKUP_DIR="/home/ageorge/Documents/Backups"
-BACKUP_DIR2="/media/ageorge/Nvme500Data/Update Backups"
+BACKUP_DIR2="/mnt/Nvme500Data/Update Backups"
 REMOTE_LOG="/home/ageorge/Desktop/remote_update.log"
 CHANGELOG_FILE="$BACKUP_DIR/changelog.txt"
 LOG_FILE="/tmp/local_update.log"
-BACKUP_LOG_DIR="$HOME/Desktop"
-BACKUP_REMOTE_LOG="$BACKUP_LOG_DIR/remote_update.log"
+pihole_log="$BACKUP_LOG_DIR/remote_update.log"
+pihole2_log="$BACKUP_LOG_DIR/remote_update2.log"
+AG_Backup_log="$BACKUP_LOG_DIR/remote_update3.log"
 BACKUP_LOG_FILE="$BACKUP_LOG_DIR/local_update.log"
-REMOTE_SCRIPT_LOCAL="/tmp/remote_update.sh"
-REMOTE_SCRIPT_REMOTE="/tmp/remote_update.sh"
+pihole_local="/tmp/remote_update.sh"
+pihole2_local="/tmp/remote_update2.sh"
+AG_Backup_local="/tmp/remote_update3.sh"
+pihole_Remote="/tmp/remote_update.sh"
+pihole2_Remote="/tmp/remote_update2.sh"
+AG_Backup_Remote="/tmp/remote_update3.sh"
 SUDO_ASKPASS_PATH="$HOME/sudo_askpass.sh"
 RUN_LOG="/tmp/run_log.txt"
 CHECKSUM_FILE="/tmp/remote_update.sh.md5"
@@ -25,6 +33,7 @@ SEEN_ERRORS_FILE="$BACKUP_LOG_DIR/seen_errors.log"
 CACHE_DIR="$HOME/.logscan_cache"
 temp_error_counts="$CACHE_DIR/temp_error_counts.txt"
 LAST_RUN_FILE="$CACHE_DIR/last_run"
+DRY_RUN=false
 
 # Function to get the last position from a cached file
 get_last_position() {
